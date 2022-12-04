6b6b467e721b659f949f0fdbbcfdbb6e03443795
shamimfl <fl.shamim69@gmail.com>
2022-12-04 16:03:55 +0600

    added require auth

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import Restaurant from './Components/Restaurant/Restaurant';
 import SingUp from './Components/SingUp/Singup';
 import Update from './Components/Update/Update';
 import Navbar from './Navbar/Navbar';
+import RequireAuth from './RequireAuth';
 
 
 function App() {
@@ -28,24 +29,26 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/rooms' element={<AddRooms/>}></Route>
+        <Route path='/rooms' element={<AddRooms />}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/all_gallery' element={<AllGallery></AllGallery>}></Route>
         <Route path='/sing_up' element={<SingUp></SingUp>}></Route>
         <Route path='/update' element={<Update></Update>}></Route>
         <Route path='/add_gallery' element={<AddGallery></AddGallery>}></Route>
         <Route path='/restaurant' element={<Restaurant></Restaurant>}></Route>
-        <Route path='/book/:_id' element={<Details></Details>}></Route>
+        <Route path='/book/:_id' element={<RequireAuth>
+          <Details></Details>
+        </RequireAuth>}></Route>
         <Route path='/checkOut' element={<CheckOut></CheckOut>}></Route>
         <Route path='/foodGallery/:food_' element={<FoodsGallery></FoodsGallery>}></Route>
         <Route path='/dashboard' element={<Dashboard></Dashboard>}>
-            <Route path='/dashboard/' element={<Charts></Charts>}></Route>
-            <Route path='/dashboard/gallery' element={<AddGallery></AddGallery>}></Route>
-            <Route path='/dashboard/add_rooms' element={<AddRooms></AddRooms>}></Route>
-            <Route path='/dashboard/add_foods' element={<AddFoods></AddFoods>}></Route>
-            <Route path='/dashboard/manage_room' element={<ManageRoom></ManageRoom>}></Route>
-            <Route path='/dashboard/add_review' element={<AddReview></AddReview>}></Route>
-            <Route path='/dashboard/foods' element={<FoodsRequest></FoodsRequest>}></Route>
+          <Route path='/dashboard/' element={<Charts></Charts>}></Route>
+          <Route path='/dashboard/gallery' element={<AddGallery></AddGallery>}></Route>
+          <Route path='/dashboard/add_rooms' element={<AddRooms></AddRooms>}></Route>
+          <Route path='/dashboard/add_foods' element={<AddFoods></AddFoods>}></Route>
+          <Route path='/dashboard/manage_room' element={<ManageRoom></ManageRoom>}></Route>
+          <Route path='/dashboard/add_review' element={<AddReview></AddReview>}></Route>
+          <Route path='/dashboard/foods' element={<FoodsRequest></FoodsRequest>}></Route>
         </Route>
       </Routes>
       <Toaster />
